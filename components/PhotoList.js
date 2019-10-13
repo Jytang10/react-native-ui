04affb1bf9acf3035599cf0945b9bb52dd7613c1
James Tang <jytang10@gmail.com>
2019-10-12 20:37:41 -0700

    Added ScrollView

--- components/PhotoList.js
+++ components/PhotoList.js
@@ -1,25 +1,21 @@
-//import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import PhotoSection from './PhotoSection';
 
-// create a component
 class PhotoList extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <PhotoSection></PhotoSection>
-      </View>
+      </ScrollView>
     );
   }
 }
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
 
-//make this component available to the app
 export default PhotoList;
